68dc67e389d405121e9ea816ea051f15cf290f87
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2025-04-02 23:38:41 -0300

    feat(database): implement SQLite database connection and task management functions

---
+++ app/database.py
@@ -0,0 +1,22 @@
+import sqlite3
+from contextlib import contextmanager
+from sqlite3 import Cursor
+
+
+@contextmanager
+def get_conn(db_name: str):
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row  # return dicts instead of tuples
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def init_db(cursor: Cursor):
+    query = """CREATE TABLE IF NOT EXISTS Tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        completed INTEGER DEFAULT 0)"""
+    cursor.execute(query)
---
+++ app/tasks.py
@@ -0,0 +1,23 @@
+from sqlite3 import Cursor
+
+# class Task:
+#    id: int | None
+#    title: str
+#    description: str | None
+#    completed: int = 0
+
+
+def get_tasks(cursor: Cursor) -> list[dict[str, str | int]]:
+    query = """SELECT * FROM Tasks"""
+    cursor.execute(query)
+    tasks = cursor.fetchall()
+    return tasks
+
+
+def add_task(cursor: Cursor, title: str, description: str | None):
+    query = "INSERT INTO Tasks(title, description, completed) VALUES (?, ?, ?)"
+    cursor.execute(query, (title, description, 0))
+
+
+def update_task(cursor: Cursor):
+    cursor.execute("")
