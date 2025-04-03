3eb056ed522fd49f0419e70406d660ca7a467f33
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2025-04-03 00:17:45 -0300

    feat(tasks): implement task update, completion, and deletion functions

--- app/tasks.py
+++ app/tasks.py
@@ -20,5 +20,16 @@ def add_task(cursor: Cursor, title: str, description: str | None):
     cursor.execute(query, (title, description, 0))
 
 
-def update_task(cursor: Cursor):
-    cursor.execute("")
+def update_task(cursor: Cursor, task_id: int, new_title: str, new_desc: str):
+    query = "UPDATE Tasks SET title = ?, description = ? WHERE id = ?"
+    cursor.execute(query, (new_title, new_desc, task_id))
+
+
+def set_completed(cursor: Cursor, task_id: int):
+    query = "UPDATE Tasks SET completed = 1 WHERE id = ?"
+    cursor.execute(query, (task_id,))
+
+
+def delete_task(cursor: Cursor, task_id: int):
+    query = "DELETE FROM Tasks WHERE id = ?"
+    cursor.execute(query, (task_id,))
