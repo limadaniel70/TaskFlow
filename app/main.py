5a01971dc5a57f51a98a63387d956cbae8a52e6c
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2025-04-03 14:57:26 -0300

    feat: implement main entry point for TaskFlow application

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 # This project is licensed under the MIT License - see the LICENSE file for details.
-from database import get_conn, init_db
-from tasks import add_task, get_tasks
+import sys
 
-DB_NAME = "taskflow.db"
+from PySide6.QtWidgets import QApplication  # pylint: disable = no-name-in-module
+from GUI import MainWindow
 
-tasks = [
-    {"title": "read", "desc": "read 1984"},
-    {"title": "study", "desc": "study math"},
-    {"title": "exercise", "desc": "go for a run"},
-    {"title": "code", "desc": "work on Python project"},
-]
 
-with get_conn(DB_NAME) as conn:
-    cursor = conn.cursor()
-    tasks = get_tasks(cursor)
-    for task in tasks:
-        print(f"Title: {task["title"]}\nDescription: {task["description"]}")
+def main():
+    """
+    TaskFlow entry point.
+    """
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
